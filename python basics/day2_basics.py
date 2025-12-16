ad4144ab7cc8dbed006f91b5844851ef9599deb3
zeenath-unnisa <zee.nisa89@gmail.com>
2025-12-17 03:42:15 +1100

    Add files via upload

---
+++ python basics/day2_basics.py
@@ -0,0 +1,35 @@
+
+# Day 2 - Python basics with C
+
+# 1. Variables: storing information about you
+name = "Zee - Yannik's mama"
+age = 36
+
+print("My name is:", name)
+print("My age is:", age)
+
+
+# 2. Simple maths with variables
+x = 10
+y = 3
+
+sum_xy = x + y
+difference = x - y
+product = x * y
+quotient = x / y
+floor = x // y
+
+print("x + y =", sum_xy)
+print("x - y =", difference)
+print("x * y =", product)
+print("x / y =", quotient)
+print("x // y =", floor)
+
+
+# 3. Tiny time helper
+hours = 20
+minutes = hours * 60
+print(hours, "hours is", minutes, "minutes.")
+
+
+      
