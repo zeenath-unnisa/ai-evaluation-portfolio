f84d16edeada9fbd748b501ea53cc13d4fd08d1d
zeenath-unnisa <zee.nisa89@gmail.com>
2025-12-17 03:38:20 +1100

    Delete day1_hello.py

--- day1_hello.py
+++
@@ -1,4 +0,0 @@
-# my first script with C
-print("Hello from a saved file.")
-print("This is my first week as an AI-tech apprentice.")
-print("C, Im ready for my next day in the plan. I did not set up one drive, i beleive it is not mine and these scripts are saved in the other account.")
